5a15ef2663f5562e0d896ff9c0c879fee9d93d1e
Moses <mosesadukwu1.2@gmail.com>
2022-12-08 20:57:14 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
+
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
- */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
