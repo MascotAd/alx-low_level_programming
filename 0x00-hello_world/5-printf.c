8589f4965963e6f1ed3c7b2634ef294059d47a74
Moses <mosesadukwu1.2@gmail.com>
2022-12-08 20:38:22 -0800

    Size

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (SUccess)
+ */
+int main(void)
+{
+	printf("which proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+
+}
