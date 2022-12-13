db69c1bdf02189fa4ff5d86cc3841b0cb5f9683f
Moses <mosesadukwu1.2@gmail.com>
2022-12-12 19:07:03 -0800

    Art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 1 (SUccess)
+ *
+ * Description: prints a string to the standard error
+ *
+ * Return: Always 1(Error)
  */
+
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
+	write(2,  err, 59);
 	return (1);
 }
