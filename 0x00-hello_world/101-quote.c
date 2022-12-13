376fa91f9a994dc70abeb346f196f5ec98d15d3d
Moses <mosesadukwu1.2@gmail.com>
2022-12-12 21:02:57 -0800

    Art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
  *
  * Description: prints a string to the standard error
  *
- * Return: Always 1(Error)
+ * Return: Always 1 (Error)
  */
 
 int main(void)
 {
 	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write (2,  err, 59);
+	write(2,  err, 59);
 	return (1);
 }
