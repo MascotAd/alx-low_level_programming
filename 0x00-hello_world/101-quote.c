02f983ae49f5544189106bcd6e788b7049eb8705
Moses <mosesadukwu1.2@gmail.com>
2022-12-12 20:25:33 -0800

    Art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,6 +12,6 @@
 int main(void)
 {
 	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
-	write(2,  err, 59);
+	write(2,  err, "and that piece of art is useful");
 	return (1);
 }
