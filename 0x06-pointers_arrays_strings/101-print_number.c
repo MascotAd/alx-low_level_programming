12d1457f9df7c41c90b21583374364ccb39a003a
Moses <mosesadukwu1.2@gmail.com>
2022-12-24 18:02:11 -0800

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,136 @@
+#include <stdio.h>
 #include "main.h"
 
+
+
 /**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
- */
 
-void print_number(int n)
+ * exponent - x to the power of y
+
+ * @x: base number
+
+ * @y: exponent
+
+ * Description: calcuates x^y
+
+ * Return: x^y
+
+ *
+
+ **/
+
+
+
+int exponent(int x, int y)
+  
 {
-	unsigned int n1;
+  
+  int power;
+  
 
-	n1 = n;
+  
+  power = x;
+  
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n1 = -n;
-	}
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+  
+  if (x == 0)
+    
+    return (0);
+  
+  if (y == 0)
+    
+    return (1);
+  
+
+  
+  while (y >= 2)
+    
+    {
+      
+      power  = power * x;
+      
+      y--;
+      
+    }
+  
+  return (power);
+  
+}
+
+
+
+/**
+
+ * print_number - print an int using only _putchar
+
+ * @number: int to be printed by function
+
+ *
+
+ * Return: nothing
+
+ **/
+
+
+
+void print_number(int number)
+  
+{
+  
+  int size, digit, counter, sign;
+  
+
+  
+  sign = 1;
+  
+  digit = 0;
+  
+  size = 1;
+  
+  counter = number;
+  
+
+  
+  if (number < 0)
+    
+    {
+      
+      _putchar('-');
+      
+      sign = -1;
+      
+    }
+  
+
+  
+  for (; counter >= 10 || counter <= -10; size++)
+    
+    {
+      
+      counter = counter / 10;
+      
+    }
+  
+
+  
+  counter = number;
+  
+
+  
+  while (size >= 2)
+    
+    {
+      
+      digit = (counter / exponent(10, size - 1)) * sign;
+      
+      _putchar(digit + '0');
+      
+      counter = counter % exponent(10, size - 1);
+      
+      size--;
+      
+    }
+  
+  _putchar(sign * counter % 10 + '0');
+  
 }
