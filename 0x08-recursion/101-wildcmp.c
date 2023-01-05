1d8d816e15c03fb2fad88eea48afee2c2a572ded
Moses <mosesadukwu1.2@gmail.com>
2023-01-05 07:08:56 -0800

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,17 +4,21 @@
  * wildcmp - A function that compares two strings
  * @s1: First input string
  * @s2: Second input string
+ * 
  * Return: 1 if strings identical and 0 in otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (!*s1 && !*s2)
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
-	if (*s2 == '*' && *(s1 + 1) && *s2)
-		return (0);
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
 	return (0);
 }
