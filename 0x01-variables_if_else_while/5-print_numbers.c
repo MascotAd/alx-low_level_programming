5ff4d876a8044d97b06a0b72532da61584ffb97c
Moses <mosesadukwu1.2@gmail.com>
2023-02-18 14:18:45 -0800

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,13 +9,12 @@
  */
 
 int main(void)
-{ 
-  int i;
- 
-  for (i = 0; i < 10; i++)
-  printf("%d", i);
-  printf("\n");
-  
-  return (0);
+{
+	int i;
 
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
 }
