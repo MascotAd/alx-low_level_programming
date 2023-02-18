e68bc3d41926e90dd0afb02cce42b9b30b921d88
Moses <mosesadukwu1.2@gmail.com>
2023-02-18 14:09:09 -0800

    Print Alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,15 +11,16 @@
 int main(void)
 {
 	char low, e, q;
+
 	e = 'e';
 	q = 'q';
-       
+
 	for (low = 'a'; low <= 'z'; low++)
 	{
 		if (low != e && low != q)
 			putchar(low);
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
