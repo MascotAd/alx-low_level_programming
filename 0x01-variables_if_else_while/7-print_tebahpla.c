813bf7d058308dad45c6cabb404727c5d6edb15b
Moses <mosesadukwu1.2@gmail.com>
2023-02-18 14:31:57 -0800

    Print tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,12 +10,11 @@
 
 int main(void)
 {
-  char low;
-  
-  for (low = 'z'; low >= 'a'; low--)
-  putchar(low);
-  putchar('\n');
-    
-  return (0);
-  
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	putchar('\n');
+	
+	return (0);
 }
