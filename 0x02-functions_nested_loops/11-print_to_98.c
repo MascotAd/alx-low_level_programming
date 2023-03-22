ea36d4bf21b718c8af9b586961cebf0a14cf897a
MascotAd <mosesadukwu1.2@gmail.com>
2023-03-22 15:02:27 +0000

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
-			printf(",");
+			printf(", ");
 		}
 		printf("\n");
 	}
@@ -30,7 +30,7 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
-			printf(",");
+			printf(", ");
 		}
 		printf("\n");
 	}
