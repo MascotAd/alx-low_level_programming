7a838a949b886ec5b0ccb5f56059add4082a668d
MascotAd <mosesadukwu1.2@gmail.com>
2023-03-22 14:24:28 +0000

    8-24 hours Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,13 +9,13 @@ void jack_bauer(void)
 {
 	int H, M;
 
-	for (H - 0; H < 24; H++)
+	for (H = 0; H < 24; H++)
 	{
 		for (M = 0; M < 60; M++)
 		{
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
-			_putchar (':');
+			_putchar(':');
 			_putchar((M / 10) + '0');
 			_putchar((M % 10) + '0');
 			_putchar('\n');
