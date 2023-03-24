a35146840c3bed4c3c79ba15a0e9c9eb5a939bc3
MascotAd <mosesadukwu1.2@gmail.com>
2023-03-24 17:47:04 +0000

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,9 @@ void more_numbers(void)
 		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
-				_putchar(i / 10 + '0');
+				_putchar((i / 10) + '0');
 			
-			_putchar(i % 10 + '0');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
