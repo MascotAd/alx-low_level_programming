9911f9426f8cc8aa1eba5fed3eae90d08a8f29c5
MascotAd <mosesadukwu1.2@gmail.com>
2023-03-25 22:53:11 +0000

    Print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,18 +7,23 @@
  */
 void print_diagonal(int n)
 {
-	int i = 0; j;
+	int x, y;
 
-	if (n > 0);
+	if (n <= 0)
 	{
-		for (; i < n; i++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (x =0; x < n; x++)
 		{
-			for (j = 0; j < i; j++)
-				_putchar(' ');
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
+
 			_putchar(92);
 			_putchar('\n');
 		}
 	}
-	else
-		_putchar('\n');
 }
