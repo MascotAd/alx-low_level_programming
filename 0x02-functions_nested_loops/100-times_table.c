53188db808ff8696987dc8811bfac2d8e9faf4fc
MascotAd <mosesadukwu1.2@gmail.com>
2023-03-26 01:23:39 +0000

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,56 @@
 #include "main.h"
 
 /**
- * print_times_table - prints times table for numbers from 0-14
+ * print_times_table - prints time table for numbers from 0-14
  * @n: An input integer value
  * Return: Nothing
  */
 void print_times_table(int n)
 {
-	int x, y, z;
+	int i, j;
 
-	if (n >= 0; x <= n; x++)
+	if (n > 0 && n < 15)
 	{
-		for (x = 0; x <= n; x++)
+		for (i = 0; i <= n; i++)
 		{
-			for (y = 0; y <= n; y++)
-			{
-				z = x * y;
-				if (z > 99)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else if (z > 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else
-				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
-				}
-			}
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+				putformat(i * j);
 			_putchar('\n');
 		}
+	}
+}
+
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
+void putformat(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
 
